9a8f2e5a0fb564ab915f5b3953e4017918990b6f
ahmetbudak <ahmet1.budak@gmail.com>
2022-02-28 12:28:18 +0300

    files changed

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN="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"
+git clone https://$TOKEN@github.com/atbudak/aws_capstone.git
+cd /home/ubuntu/aws_capstone
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/aws_capstone/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
